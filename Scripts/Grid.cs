2b790b269c8d06930ad5ab68c265101a8dc2c3d9
Jee Van Kur <40460588+Xhiwankur@users.noreply.github.com>
2019-03-26 04:47:20 +0530

    added scripts

---
+++ Scripts/Grid.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour {
+
+	Node[,] grid;
+	public List<Node> path;
+	public LayerMask unwalkableMask;
+	public Vector2 gridWorldSize;
+	public float nodeDiameter;
+	int gridSizeX, gridSizeY;
+	void Start()
+	{
+// find grid size
+		gridSizeX = Mathf.RoundToInt(gridWorldSize.x/nodeDiameter);
+		gridSizeY = Mathf.RoundToInt(gridWorldSize.y/nodeDiameter);
+		Creategrid();
+
+	}
+
+	void Creategrid() {
+		grid = new Node[gridSizeX,gridSizeY];
+// Set reference
+		Vector3 worldBottomLeft = transform.position - Vector3.right*gridWorldSize.x/2 - Vector3.up*gridWorldSize.y/2;
+// Check for obstacles
+		for (int x = 0; x<gridSizeX; x++){
+			for (int y = 0; y<gridSizeY; y++){
+// position of current node
+				Vector3 worldPoint = worldBottomLeft + Vector3.right*(x*nodeDiameter + nodeDiameter/2) + Vector3.up*(y*nodeDiameter+nodeDiameter/2);
+// check if node is walkable
+				bool walkable = !(Physics2D.OverlapCircle(worldPoint, nodeDiameter/2, unwalkableMask));
+				bool visited = false;
+// add node to grid
+				grid[x,y] = new Node(walkable,worldPoint, x, y, visited);
+			}
+		}
+	}
+	
+// dynamic update targets to avoid overlapping
+	public void GridUpdate(){
+		for (int x = 0; x<gridSizeX; x++){
+			for (int y = 0; y<gridSizeY; y++){
+				grid[x,y].walkable = !(Physics2D.OverlapCircle(grid[x,y].worldPosition, nodeDiameter/2, unwalkableMask));
+			}
+		}
+	}
+
+public List<Node> GetNeighbours(Node node){
+	List<Node> neighbours = new List<Node>();
+	for (int x = -1; x<=1; x++){
+		for (int y = -1; y<=1; y++){
+			if (x == 0 && y == 0)
+				continue;
+// coordinates(grid) of the subject
+			int checkX = node.gridX + x;
+			int checkY = node.gridY + y;
+// add valid neighbours
+			if (checkX >= 0 && checkX < gridSizeX && checkY >= 0 && checkY < gridSizeY){
+				neighbours.Add(grid[checkX,checkY]);
+			}
+		}
+	}
+	return neighbours;
+}
+public Node NodeFromPoint(Vector3 point){
+	float percentX = Mathf.Clamp01((point.x + gridWorldSize.x/2)/gridWorldSize.x);
+	float percentY = Mathf.Clamp01((point.y + gridWorldSize.y/2)/gridWorldSize.y);
+	int x = Mathf.RoundToInt(percentX*(gridSizeX-1)); // -1 to avoid goind out of bounds
+	int y = Mathf.RoundToInt(percentY*(gridSizeY-1));
+	return grid[x,y];
+
+}
+
+// draw the grid
+	void OnDrawGizmos(){
+		int i = 0;
+		Gizmos.DrawWireCube(transform.position, new Vector3(gridWorldSize.x, gridWorldSize.y, 1));
+		if(grid != null){
+			foreach (Node n in grid){
+				i++;
+				Gizmos.color = (n.walkable)?Color.white:Color.red;
+				if(path != null){
+					if (path.Contains(n)){
+						Gizmos.color = Color.black;
+					}
+				}
+				Gizmos.DrawCube(n.worldPosition, Vector3.one*(nodeDiameter-0.1f));
+			}
+		}
+	}
+}
---
+++ Scripts/PathPlanner.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathPlanner : MonoBehaviour {
+
+	Grid gridscript;
+	PebbleManager pebbleScript;
+
+
+
+	void Awake(){
+		gridscript = GetComponent<Grid>();
+		pebbleScript = GetComponent<PebbleManager>();
+	}
+
+	public void StartFindPath(Vector2 startPos, Vector2 targetPos){
+// update grid with new features and start pathplanning
+		gridscript.GridUpdate();
+		StartCoroutine(FindPath(startPos, targetPos));
+	}
+// impliment A*
+	IEnumerator FindPath(Vector2 startPos, Vector2 endPos){
+
+		Vector2[] wayPoints = new Vector2[0];
+		bool pathSuccess = false;
+
+		Node startNode = gridscript.NodeFromPoint(startPos);
+		Node targetNode = gridscript.NodeFromPoint(endPos);
+
+		List<Node> openSet = new List<Node>();
+		HashSet<Node> closedSet = new HashSet<Node>();
+		openSet.Add(startNode);
+// loop A*
+		while (openSet.Count > 0){
+			Node currentNode = openSet[0];
+			for (int i = 1; i<openSet.Count; i++){
+				if((openSet[i].fCost < currentNode.fCost && !openSet[i].visited ) || (openSet[i].fCost == currentNode.fCost && openSet[i].hCost<currentNode.hCost   && !openSet[i].visited)) {
+					currentNode = openSet[i];
+				}
+			}
+// update nodes
+			openSet.Remove(currentNode);
+			closedSet.Add(currentNode);
+// if finish
+			if (currentNode == targetNode) {
+				pathSuccess = true;
+				break;
+			}
+// neighbouring nodes
+			foreach (Node neighbour in gridscript.GetNeighbours(currentNode)){
+				if(!neighbour.walkable || closedSet.Contains(neighbour)){
+					continue;
+				}
+// calculate and check neighbour costs
+				int newMovementCostToNeighbour = currentNode.gCost + GetDistance(currentNode, neighbour);
+				if ( ( newMovementCostToNeighbour < neighbour.gCost || !openSet.Contains(neighbour)) && !neighbour.visited){
+					neighbour.gCost = newMovementCostToNeighbour;
+					neighbour.hCost = GetDistance(neighbour, targetNode);
+					neighbour.parent = currentNode;
+
+					if (!openSet.Contains(neighbour))
+						openSet.Add(neighbour);
+				}
+			}
+		}
+		yield return null;
+		if(pathSuccess){
+			wayPoints = RetracePath(startNode, targetNode);
+			GameObject.Find("player").GetComponent<PlayerController>().PathUpdate(wayPoints, pathSuccess);
+			Debug.Log("Path Updated");
+		}
+	}
+
+	Vector2[] RetracePath(Node startNode, Node endNode)
+	{
+		List<Node> path = new List<Node>();
+		List<Vector2> wayPoints = new List<Vector2>();
+		Node currentNode = endNode;
+// update path and vector waypoints
+		while (currentNode != startNode){
+			path.Add(currentNode);
+			pebbleScript.PlacePebbles(path);
+			wayPoints.Add(currentNode.worldPosition);
+			currentNode = currentNode.parent;
+		}
+// reverse path from start to end
+		path.Reverse();
+		wayPoints.Reverse();
+		gridscript.path = path;
+		return wayPoints.ToArray();
+	}
+
+	int GetDistance(Node nodeA, Node nodeB) {
+		int dstX = Mathf.Abs(nodeA.gridX - nodeB.gridX);
+		int dstY = Mathf.Abs(nodeA.gridY - nodeB.gridY);
+
+		if (dstX > dstY)
+			return 14*dstY + 10*(dstX-dstY);
+		return 14*dstX + 10*(dstY-dstX); 
+	}
+
+}
