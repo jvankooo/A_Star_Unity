33beea0283e3e5e9438c3f50c02cd70360b593dc
J Van K <jeevankur.iit@gmail.com>
2019-10-31 01:53:19 +0530

    added test final project

---
+++ Unity_Project/Assets/Scripts/SceneController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour {
+
+	public void Mainmenu(){
+		SceneManager.LoadScene(0);
+	}
+
+	public void RapidProt(){
+		SceneManager.LoadScene(1);
+	}
+
+	public void Final(){
+		SceneManager.LoadScene(2);
+	}
+
+	public void Endgame(){
+		Application.Quit();
+	}
+
+}
